e3e9abf760710ae3ec267c7fb6d12710deadfd94
willhadair <willhadair@gmail.com>
2025-05-18 13:53:14 -0500

    feat: Enhance Settings Page with Dark Mode toggle and layout improvements

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:workout_app/components/custom_switch.dart';
+import 'package:workout_app/components/text_divider.dart';
+import 'package:workout_app/utils/settings.dart';
 import 'package:workout_app/utils/themes.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -12,9 +15,18 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
+    final settingsProvider = Provider.of<SettingsProvider>(context);
     final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
       backgroundColor: themeProvider.getBackgroundColor(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const TextDivider(text: "Theme", icon: Icons.light_mode),
+          //TODO: SAVE STATE FOR DARK MODE
+          CustomSwitch(title: "Dark Mode", defaultValue: settingsProvider.isDarkMode, onChangeMethod: (bool newValue) => themeProvider.toggleTheme(context, newValue))
+          ],
+      ),
     );
   }
 }
