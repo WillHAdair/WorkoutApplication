bf9224de11a6b4b34a9658a312fab82b5b7b406a
willhadair <willhadair@gmail.com>
2026-03-14 17:13:56 -0500

    begin schedule day work

--- WorkoutApp.API/Models/ScheduleDay.cs
+++ WorkoutApp.API/Models/ScheduleDay.cs
@@ -4,13 +4,8 @@ public abstract class ScheduleDay : NamedEntity
 {
     public required Guid WorkoutScheduleId { get; set; }
     public WorkoutSchedule? WorkoutSchedule { get; set; }
-}
 
-public class RestDay : ScheduleDay
-{
-}
-
-public class WorkoutDay : ScheduleDay
-{
+    // Empty collection is assumed to mean rest day
     public IEnumerable<WorkoutExercise> WorkoutExercises { get; set; } = [];
 }
+
